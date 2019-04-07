0100a18ab4d4d24545e1118ac6269eeb591e03bb
alterclown <rahi2721@gmail.com>
2019-04-07 04:54:55 -0700

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './app/add-book/add-book.component';
+
+import { AddReaderComponent } from './add-reader/add-reader.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
+import { EditReaderComponent } from './edit-reader/edit-reader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+   
+    AddReaderComponent,
+    DashboardComponent,
+    EditBookComponent,
+    EditReaderComponent
   ],
   imports: [
     BrowserModule,
